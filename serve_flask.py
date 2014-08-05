85090b7abeb88fd3c0ea276c2dc800d70be02f2d
eugene <echinveeraphan@rivierapartners.com>
2014-08-04 17:18:42 -0700

    google auth bug fix

--- serve_flask.py
+++ serve_flask.py
@@ -41,8 +41,7 @@ def login(provider_name='google'):
                     print response
             if result.user.email:
                 print result.user.data
-                session['user_name'] = result.user.data
-                #session['user_name'] = result.user.data.name.displayName
+                session['user_name'] = result.user.data.displayName
                 session['user_email'] = result.user.email
         return render_template('movie_poll.html',
                             user=session['user_name'],
