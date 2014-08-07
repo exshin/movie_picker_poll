cb044af42b2de6ea0c824047bcebd2258fd11943
eugene <echinveeraphan@rivierapartners.com>
2014-08-07 16:03:12 -0700

    bug fixes

--- movie_picker.py
+++ movie_picker.py
@@ -40,7 +40,7 @@ def vote(movie, user_email=None, imdb_id=None):
 			movie_info = get_movie_info(movie.lower())
 		print movie_info.get('Plot')
 		conn, dbCursor = connect_db()
-		dbCursor.execute(sql_write_vote,[movie,user_email])
+		dbCursor.execute(sql_write_vote,[movie_info.get('Title'),user_email])
 		conn.commit()
 		if movie_info:
 			movie_poster_url = get_poster(movie_info.get('imdbID'))
